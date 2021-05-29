f4685fd5f6daed42a86096b91c33728d2aacd2d2
gautam404 <84016907+gautam404@users.noreply.github.com>
2021-05-29 14:12:53 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 app = express(); // Initializing app
   
 // Creating a cron job which runs on every 10 second
-cron.schedule("*/5 * * * * *", function() {
+cron.schedule("*/7 * * * * *", function() {
 	var axios = require('axios');
 const now = new Date((new Date()).getTime() + 1000 * 3600 * 18);
     const day = ("0" + now.getDate()).slice(-2);
@@ -12,7 +12,7 @@ const now = new Date((new Date()).getTime() + 1000 * 3600 * 18);
     const today = (day) +"-" + (month) +"-"+now.getFullYear() ;
 var config = {
   method: 'get',
-  url: 'https://telectronics.in/CoAPI/covid.php',
+  url: 'http://3.108.87.80/covid.php',
   headers: { 
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
   }
@@ -25,7 +25,7 @@ axios(config)
 .catch(function (error) {
   console.log(error);
 });
-    console.log("running a task every 5 second");
+    console.log("running a task every 7 second");
 });
   
 app.listen(3000);
