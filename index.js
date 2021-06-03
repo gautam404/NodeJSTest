4fc67ffdc37dd0fb11215b8d92cc594b3ba2e2db
gautam404 <84016907+gautam404@users.noreply.github.com>
2021-06-03 17:59:27 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const express = require("express");
 app = express(); // Initializing app
   
 // Creating a cron job which runs on every 10 second
-cron.schedule("*/5 * * * * *", function() {
+cron.schedule("*/6 * * * * *", function() {
 	var axios = require('axios');
 
-var config = {
-  method: 'get',
-  url: 'http://3.108.87.80/covid.php',
-  headers: { 
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
-  }
-};
+	var config = {
+	  method: 'get',
+	  url: 'http://3.108.87.80/covid.php',
+	  headers: { 
+	    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+	  }
+	};
 
-axios(config)
-.then(function (response) {
-	  console.log(response.data);
-})
-.catch(function (error) {
-  console.log(error);
+	axios(config)
+	.then(function (response) {
+		  console.log(response.data);
+	})
+	.catch(function (error) {
+	  console.log(error);
+	});
+    console.log("running a task every 6 second");
 });
-    console.log("running a task every 5 second");
-});
-  
+
 app.listen(process.env.PORT || 5000);
