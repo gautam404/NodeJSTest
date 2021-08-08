b51e28c7e79b9140a043601d6b130890dc295b8d
gautam404 <84016907+gautam404@users.noreply.github.com>
2021-08-08 12:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 app = express(); // Initializing app
   
 // Creating a cron job which runs on every 10 second
-cron.schedule("*/30 * * * * *", function() {
+cron.schedule("*/7 * * * * *", function() {
 	var axios = require('axios');
 
 	var config = {
@@ -22,7 +22,7 @@ cron.schedule("*/30 * * * * *", function() {
 	.catch(function (error) {
 	  console.log(error);
 	});
-    console.log("running a task every 30 second");
+    console.log("running a task every 7 second");
 });
 
 app.listen(process.env.PORT || 5000);
